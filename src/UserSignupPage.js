a0228cc0bd3449ea7a7b15d0be38cd6c29dc40b4
cumalitezcan <cumalitezcan@gmail.com>
2020-12-25 18:24:47 +0300

    userInteraction

--- src/UserSignupPage.js
+++ src/UserSignupPage.js
@@ -2,25 +2,51 @@
 import React, { Component } from 'react'
 
 class UserSignupPage extends Component {
+
+    state = {
+        username:null,
+        displayName:null,
+        password:null,
+        passwordRepeat:null
+    };
+
+    /* onChangeUserName = event => {
+        this.setState({
+            username: event.target.value
+        });
+    } */
+
+    onChange = event => {
+        /* const value = event.target.value;
+        const name = event.target.name; */
+
+        //object destructuring
+        const {name,value} = event.target;
+
+        this.setState({
+            [name]:value
+        })
+    }
+
     render() {
         return (
             <form>
                 <h1>Sign Up</h1>
                 <div>
                     <label>Username</label>
-                    <input />
+                    <input name="username" onChange={this.onChange} />
                 </div>
                 <div>
                     <label>Display Name</label>
-                    <input />
+                    <input name="displayName" onChange={this.onChange} />
                 </div>
                 <div>
                     <label>Password</label>
-                    <input type="password" />
+                    <input name="password" onChange={this.onChange} type="password" />
                 </div>
                 <div>
                     <label>Password Repeat</label>
-                    <input type="password" />
+                    <input name="passwordRepeat" onChange={this.onChange} type="password" />
                 </div>
                 <button>Sign Up</button>
 
