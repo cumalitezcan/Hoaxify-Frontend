29c405c7458c4b9128dbe600297203ddcb242632
cumalitezcan <cumalitezcan@gmail.com>
2020-12-25 16:18:18 +0300

    firstHeader

---
+++ src/UserSignupPage.js
@@ -0,0 +1,14 @@
+
+import React, { Component } from 'react'
+
+class UserSignupPage extends Component {
+    render() {
+        return (
+            <h1>
+                Sign Up
+            </h1>
+        )
+    }
+}
+
+export default UserSignupPage;
--- src/index.js
+++ src/index.js
@@ -1,12 +1,13 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
+/* import App from './App'; */
+import UserSignupPage from '../src/UserSignupPage';
 import reportWebVitals from './reportWebVitals';
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <UserSignupPage />
   </React.StrictMode>,
   document.getElementById('root')
 );
